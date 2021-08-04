9a50e876398be3551a4c26c675a0b34c9955d7a0
Lunerio <lunerio@outlook.es>
2021-08-03 21:09:58 -0300

    save lightning and new model

--- public/script.js
+++ public/script.js
@@ -1,37 +1,40 @@
 // import * as THREE from './threejs-loader/three.js/build/three.js';
 import * as OrbitControls from './threejs-loader/three.js/examples/js/controls/OrbitControls.js';
 import * as GLTFLoader from './threejs-loader/three.js/examples/js/loaders/GLTFLoader.js';
-// import { GUI } from './threejs-loader/three.js/examples/jsm/libs/dat.gui.module.js';
+import { GUI } from './threejs-loader/three.js/examples/jsm/libs/dat.gui.module.js';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('#c8deff');
 
-const color = 0xFFEBAD;
-const intensity = 3;
-const light = new THREE.DirectionalLight(color, intensity);
-light.position.set(0, 10, 0);
-light.target.position.set(-9, 2, 4.7);
+const color = 0xf9c970;
+const intensity = 3.8;
+const distance = 0;
+const light = new THREE.PointLight(color, intensity, distance);
+light.position.set(-1.81, 2.1, 3.8);
 scene.add(light);
-scene.add(light.target);
 
 // const gui = new GUI();
-// gui.add(light, 'intensity', 0, 2, 0.01);
-// gui.add(light.target.position, 'x', -10, 10);
-// gui.add(light.target.position, 'z', -10, 10);
-// gui.add(light.target.position, 'y', 0, 10);
+// gui.add(light, 'intensity', 0, 20, 0.01);
+// gui.add(light.position, 'x', -10, 10);
+// gui.add(light.position, 'z', -10, 10);
+// gui.add(light.position, 'y', 0, 10);
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = -0.2;
-camera.position.y = 1;
-camera.position.x = 2.2;
-const renderer = new THREE.WebGLRenderer();
+camera.position.z = 4;
+camera.position.y = 2;
+camera.position.x = 1;
+
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new THREE.OrbitControls( camera, renderer.domElement );
 
 const loader = new THREE.GLTFLoader();
-loader.load('/blender-files/baseModel.gltf', function (gltf) {
+loader.load('/blender-files/baseModel.glb', function (gltf) {
+    console.log(gltf);
     scene.add(gltf.scene);
 });
 
