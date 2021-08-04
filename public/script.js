d9328a351d8cb33839a2cba3709613d7645e4455
Lunerio <lunerio@outlook.es>
2021-08-04 00:13:01 -0300

    test path

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
-import * as OrbitControls from './threejs-loader/three/examples/js/controls/OrbitControls.js';
-import * as GLTFLoader from './threejs-loader/three/examples/js/loaders/GLTFLoader.js';
-// import { GUI } from './threejs-loader/three.js/examples/jsm/libs/dat.gui.module.js';
+import * as OrbitControls from './threejs-loader/examples/js/controls/OrbitControls.js';
+import * as GLTFLoader from './threejs-loader/examples/js/loaders/GLTFLoader.js';
+// import { GUI } from './threejs-loader/examples/jsm/libs/dat.gui.module.js';
 
 // Create a new scene and add background color
 const scene = new THREE.Scene();
